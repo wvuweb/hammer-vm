6e4fb9f6ce1ccc7a039c67b42d400280fcb4334c
Steven Spriggs <steven.spriggs@gmail.com>
2015-02-27 17:21:38 -0500

    git needs to run with sudo

--- hammer/update.rb
+++ hammer/update.rb
@@ -6,7 +6,7 @@ class Update < Vagrant.plugin(2, :command)
 
   def execute
     puts "Updating Hammer..."
-    `vagrant ssh -c "sudo kill -9 $(ps ax | grep '[h]ammer_server.rb' | awk '{print $1}') && cd /srv/hammer && git pull && bundle install && cd hammer && ruby hammer_server.rb --daemon 1" default`
+    `vagrant ssh -c "sudo kill -9 $(ps ax | grep '[h]ammer_server.rb' | awk '{print $1}') && cd /srv/hammer && sudo git pull && bundle install && cd hammer && ruby hammer_server.rb --daemon 1" default`
     puts "Hammer updated!"
   end
 end
