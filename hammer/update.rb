b65fcb2189e5b063ba85f9936ac3efb8f4dc781a
Steven Spriggs <steven.spriggs@gmail.com>
2015-02-27 23:06:27 -0500

    clean up update command

--- hammer/update.rb
+++ hammer/update.rb
@@ -5,8 +5,36 @@ class Update < Vagrant.plugin(2, :command)
   end
 
   def execute
-    puts "Updating Hammer..."
-    `vagrant ssh -c "sudo kill -9 $(ps ax | grep '[h]ammer_server.rb' | awk '{print $1}') && cd /srv/hammer && sudo git pull && bundle install && cd hammer && ruby hammer_server.rb --daemon 1" default`
-    puts "Hammer updated!"
+    # puts "Updating Hammer..."
+    # puts "Stopping Hammer Processes..."
+    # `vagrant ssh -c "sudo kill -9 $(ps ax | grep '[h]ammer_server.rb' | awk '{print $1}')" default -- -qT`
+    # puts "Pulling latest code from Github..."
+    # `vagrant ssh -c " cd /srv/hammer && sudo git pull" default -- -qT`
+    # puts "Running Bundle install..."
+    # `vagrant ssh -c "sudo bundle install" default -- -qT`
+    # puts "Restarting Hammer..."
+    # `vagrant ssh -c "cd /srv/hammer/hammer && ruby hammer_server.rb --daemon 1" default  -- -qT`
+    # puts "Hammer updated!"
+
+    command1 = "sudo kill -9 $(ps ax | grep '[h]ammer_server.rb' | awk '{print $1}')"
+    command2 = "cd /srv/hammer && sudo git pull"
+    command3 = "cd /srv/hammer && sudo bundle install"
+    command4 = "cd /srv/hammer/hammer && ruby hammer_server.rb --daemon 1"
+
+    with_target_vms(nil, :single_target => true) do |vm|
+      safe_puts("Stopping Hammer Processes...")
+      vm.action(:ssh_run, :ssh_run_command => command1, ssh_opts: {extra_args: ["-qT"]})
+      safe_puts("Pulling latest code from GitHub...")
+      vm.action(:ssh_run, :ssh_run_command => command2, ssh_opts: {extra_args: ["-qT"]})
+      safe_puts("Running Bundle Install...")
+      vm.action(:ssh_run, :ssh_run_command => command3, ssh_opts: {extra_args: ["-qT"]})
+      safe_puts("Restarting Hammer...")
+      vm.action(:ssh_run, :ssh_run_command => command4, ssh_opts: {extra_args: ["-qT"]})
+      safe_puts("Finished Upgrade!")
+      return 0
+    end
+
+
+
   end
 end
